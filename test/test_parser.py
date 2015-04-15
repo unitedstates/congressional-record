779dcb85377d2f5fdcb264a4a05d95de88797ca8
Jace <jaceprince84@gmail.com>
2015-04-14 19:49:36 -0600

    Updated test to pass TravisCI

--- test/test_parser.py
+++ test/test_parser.py
@@ -1,9 +1,8 @@
-import sys
 import unittest
-import os
 import re
 
 from congressionalrecord.fdsys import cr_parser
+
 
 
 # simple files to test basic boiler plate parsing
@@ -28,7 +27,7 @@ class test_results(unittest.TestCase):
     # # the parser is supposed to ignore front matter
     # def test_ignore_files(self):
     # cr_parser.parse_directory("test/test_files/front_matter", logdir="test/test_output", outdir="test/test_output")
-    # 	# make sure test_files/front_matter/__parsed doesn't exist with a sample from the House and Senate
+    # # make sure test_files/front_matter/__parsed doesn't exist with a sample from the House and Senate
     # 	self.assertTrue(not os.path.exists('test/test_output/__parsed'))
 
     # identifying speakers
