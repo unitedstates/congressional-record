6a69126e76ea51a1fb9953f137e4400d06b57680
Nick Judd <nick@nclarkjudd.com>
2018-12-27 12:01:34 -0600

    added test to catch #38

--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -19,3 +19,7 @@ class testDownloader(unittest.TestCase):
     def test_handle_existing(self):
         download = dl.Downloader('2005-07-20',do_mode='json')
         self.assertIn(download.status,['extractedFilesdeletedZip','existingFiles'])
+
+    def test_handle_empty(self):
+        download = dl.Downloader('2017-01-02',do_mode='json')
+        self.assertEqual(download.status,'downloadFailure')
